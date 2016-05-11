9e0c374a7bbbd6353072ad49357e16b2cabf99a9
fraeri <flyer1>
2016-05-11 10:43:44 +0200

    [fix] - show only active payment methods

--- app/code/community/RicoNeitzel/PaymentFilter/Helper/Data.php
+++ app/code/community/RicoNeitzel/PaymentFilter/Helper/Data.php
@@ -59,7 +59,19 @@ class RicoNeitzel_PaymentFilter_Helper_Data extends Mage_Core_Helper_Abstract
      */
     public function getPaymentMethodOptions($storeId, $quote = null)
     {
-        return Mage::helper('payment')->getPaymentMethodList(true, true, true);
+        $payments = Mage::getSingleton('payment/config')->getActiveMethods();
+
+        $methods = array(array('value'=>'', 'label'=>Mage::helper('adminhtml')->__('--Please Select--')));
+
+        foreach ($payments as $paymentCode=>$paymentModel) {
+            $paymentTitle = Mage::getStoreConfig('payment/'.$paymentCode.'/title');
+            $methods[$paymentCode] = array(
+                'label'   => $paymentTitle,
+                'value' => $paymentCode,
+            );
+
+        }
+        return $methods;
     }
 
     /**
